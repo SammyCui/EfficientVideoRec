72ecdaed07507447055142e4c648abb9e5dad368
Sammy Cui <xcui16@stlawu.edu>
2023-02-08 23:02:21 -0500

    add scripts

---
+++ scripts/main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-task=1
+#SBATCH --job-name=ImagenetR18
+#SBATCH --mail-user=xcui32@fordham.edu
+#SBATCH --mail-type=END
+#SBATCH --output=/u/erdos/cnslab/xcui32/FSL/results/mini_conv/output.out
+
+module purg
+module load gcc8 cuda10.2
+module load openmpi-geib-cuda10.2-gcc8/3.1.4
+module load pytorch-py37-cuda10.2-gcc8/1.9.1
+module load ml-pythondeps-py37-cuda10.2-gcc8/4.7.5
+
+
+python3 /u/erdos/cnslab/xcui32/FSL/main.py \
+ --root  \
+ --start_epoch 0 --max_epoch 100 \
+ --lr 0.001 --optimizer adam --lr_scheduler step --step_size 20 --gamma 0.2 \
+ --momentum 0.9  --weight_decay 0.0005 --val_interval 1 \
+ --num_workers 16 --device 'cuda:0' --download False --result_dir './results/fovcnn' --save False --resume --init_backbone
