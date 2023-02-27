add different model variant configs, fix duplicate average meters

trainer/base.py
@@ -7,7 +7,7 @@ from utils.logger import Logger
 from utils.meters import AverageMeter, StatsMeter
 from utils.metric import accuracy
 from timeit import default_timer as timer
-
+from copy import deepcopy
 from trainer.helpers import get_model_optimizer, get_dataloaders
 
 
@@ -21,7 +21,7 @@ class BaseTrainer(metaclass=abc.ABCMeta):
         self.max_epoch = args.max_epoch
         self.device = args.device
 
-        self.train_time, self.forward_tm, self.backward_tm, self.optimize_tm = (AverageMeter(),) * 4
+        self.train_time, self.forward_tm, self.backward_tm, self.optimize_tm = (deepcopy(AverageMeter()) for _ in range(4))
 
         self.result_log = {'max_val_acc@1': 0,
                            'max_val_acc@1_epoch': 0}
@@ -40,7 +40,7 @@ class BaseTrainer(metaclass=abc.ABCMeta):
 
             self.model.train()
 
-            train_loss, train_acc_1, train_acc_3, train_acc_5 = (AverageMeter(),) * 4
+            train_loss, train_acc_1, train_acc_3, train_acc_5 = (deepcopy(AverageMeter()) for _ in range(4))
 
             for batch in self.train_dataloader:
 
@@ -92,7 +92,7 @@ class BaseTrainer(metaclass=abc.ABCMeta):
 
     def _validate(self):
         self.model.eval()
-        val_loss, val_acc_1, val_acc_3, val_acc_5 = (StatsMeter(),) * 4
+        val_loss, val_acc_1, val_acc_3, val_acc_5 = (deepcopy(StatsMeter()) for _ in range(4))
         with torch.no_grad():
             for batch in self.val_dataloader:
                 if self.args.per_size:
@@ -164,7 +164,7 @@ class BaseTrainer(metaclass=abc.ABCMeta):
             self.model.load_state_dict(param)
         self.model.eval()
         t0 = timer()
-        test_loss, test_acc_1, test_acc_3, test_acc_5 = (StatsMeter(),) * 4
+        test_loss, test_acc_1, test_acc_3, test_acc_5 = (deepcopy(StatsMeter()) for _ in range(4))
         with torch.no_grad():
             for batch in self.test_dataloader:
                 if self.args.per_size:
@@ -218,8 +218,8 @@ class BaseTrainer(metaclass=abc.ABCMeta):
                 f.write('best epoch {}, best val acc={:.4f}\n'.format(
                     self.result_log['max_val_acc@1_epoch'],
                     self.result_log['max_val_acc@1']))
-                f.write('Test acc={:.4f}\n'.format(
-                    self.result_log['test_acc']))
+                f.write('Test acc@1={:.4f} acc@3={:.4f} acc@5={:.4f}\n'.format(
+                    self.result_log['test_acc@1'], self.result_log['test_acc@3'], self.result_log['test_acc@5']))
                 f.write('Total time to converge: {:.3f} hrs, per epoch: {:.5f} hrs'
                         .format(self.train_time.sum / 3600, self.train_time.avg / 3600))
 

trainer/helpers.py
@@ -1,10 +1,12 @@
 import argparse
 import os
+from typing import List
+import re
 import torch
 from torch import optim
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
-from models.vit import reducer_vit_tiny_patch16_224, vit_tiny_patch16_224
+from models.vit import vit_benchmark, reducer_vit
 
 from backbones.resnet import resnet12, resnet18
 
@@ -94,17 +96,17 @@ def get_dataloaders(args):
     # train_dataset = torchvision.datasets.CIFAR10(root='./dataset', train=True, transform=transform, download=True)
     # val_dataset = torchvision.datasets.CIFAR10(root='./dataset', train=False, transform=transform, download=True)
     train_dataset = VOCDataset(root=os.path.join(args.train_root, 'root'), anno_root=os.path.join(args.train_root, 'annotations'),
-                               cls_to_use=DEFAULT_CLS,
+                               cls_to_use=args.cls_to_use,
                                transform=transform,
                                per_size=args.per_size,
                                object_only=args.object_only)
     val_dataset = VOCDataset(root=os.path.join(args.val_root, 'root'), anno_root=os.path.join(args.val_root, 'annotations'),
-                            cls_to_use=DEFAULT_CLS,
+                            cls_to_use=args.cls_to_use,
                             transform=transform,
                             per_size=args.per_size,
                             object_only=args.object_only)
     test_dataset = VOCDataset(root=os.path.join(args.test_root, 'root'), anno_root=os.path.join(args.test_root, 'annotations'),
-                              cls_to_use=DEFAULT_CLS,
+                              cls_to_use=args.cls_to_use,
                               transform=transform,
                               per_size=args.per_size,
                               object_only=args.object_only)
@@ -123,6 +125,8 @@ def args_parser():
     parser.add_argument('--max_epoch', type=int, default=100)
     parser.add_argument('--train', type=str, default='True')
     parser.add_argument('--mode', type=str, default='reducer-img')
+    parser.add_argument('--model', type=str, nargs='?', const='Benchmark', default='Benchmark')
+    parser.add_argument('--model_variant', type=str)
     parser.add_argument('--reducer', type=str, default='RandomReducer')
     parser.add_argument('--patch_size', type=int, default=16)
 
@@ -131,7 +135,6 @@ def args_parser():
     parser.add_argument('--reducer_inner_dim', type=int, default=32)
     parser.add_argument('--keep_ratio', type=float, default=0.8)
     parser.add_argument('--backbone', type=str, nargs='?', const='resnet18', default='resnet18')
-    parser.add_argument('--model', type=str, nargs='?', const='Benchmark', default='Benchmark')
     parser.add_argument('--backbone_out_dim', type=int, nargs='?', const=512, default=512)
     parser.add_argument('--pe', type=str, nargs='?', const=None,default=None)
     parser.add_argument('--per_size', type=int, nargs='?', const=None, default=None)
@@ -164,6 +167,11 @@ def args_parser():
 
 def post_process_args(args):
 
+    if args.patch_size and args.model_variant and args.pretrained:
+        reg = re.compile("patch(\d+)")
+        pretrained_patchsize = int(reg.findall(args.model_variant)[0])
+        assert pretrained_patchsize == args.patch_size, 'Specified patch size has to be equal to the pretrained patch size'
+
     args.train_root = os.path.join(args.root, 'train')
     args.val_root = os.path.join(args.root, 'val')
     args.test_root = os.path.join(args.root, 'test')
@@ -195,6 +203,7 @@ class DebugArgs:
                  reducer_inner_dim: int = 64,
                  patch_size: int = 16,
                  backbone_out_dim: int = 512,
+                 model_variant: str = 'vit_tiny_patch16_224',
                  pe: str = None,
                  per_size: int = None,
                  concat: bool = False,
@@ -208,7 +217,8 @@ class DebugArgs:
                  lr_scheduler: str = 'step',
                  step_size: int = 20,
                  gamma: float = 0.2,
-                 num_classes: int =10,
+                 num_classes: int =20,
+                 cls_to_use: List[str] = None,
                  momentum: float = 0.9,
                  weight_decay: float = 0.0005,
                  train: bool = True,
@@ -226,8 +236,10 @@ class DebugArgs:
         self.root = root
         self.backbone = backbone
         self.mode = mode
+        self.cls_to_use = cls_to_use
         self.keep_ratio = keep_ratio
         self.patch_size = patch_size
+        self.model_variant = model_variant
         self.backbone_out_dim = backbone_out_dim
         self.pe = pe
         self.per_size = per_size